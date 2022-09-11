96ff593d8faf193ff71f1a709df034ae9e8852a9
boogie27 <anonyecharles@gmail.com>
2022-09-11 16:40:22 +0100

    weshopapp-server

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ mongoose.connect(dataBaseURL, connectParams)
 
 // testing url
 app.get('/api/demo', async (request, response) => {
-    return console.log('working properly')
+    return response.send({ data: true })
 })
 
 
